764c646bf2f1c6f99066c7fdce04e561403ea90b
Chris Myers <chris@creativelittledots.co.uk>
2017-05-09 12:29:33 +0100

    Initial commit
    Signed-off-by: Chris Myers <chris@creativelittledots.co.uk>

---
+++ cl-pf-boost.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @package cl-pf-boost
+ * @version 1.1
+ */
+/*
+Plugin Name: Creative Little Performance Boost
+
+Description: Gives a little performance boost to your wordpress site by stripping queried strings from static resource urls, removing unused scripts, and cleaning up script tags.
+Author: Creative Little Developer
+Version: 1.1
+*/
+
+function my_deregister_scripts(){
+  wp_deregister_script( 'wp-embed' );
+}
+
+function clean_script_tag($input) {
+    
+    $input = str_replace("type='text/javascript' ", '', $input);
+    
+    return str_replace("'", '"', $input);
+    
+}
+
+function remove_cssjs_ver( $src ) {
+
+    if( strpos( $src, '?ver=' ) ) {
+    
+        $src = remove_query_arg( 'ver', $src );
+    
+    }
+        
+    return $src;
+    
+}
+
+
+add_action( 'wp_footer', 'my_deregister_scripts' );
+
+add_filter( 'script_loader_tag', 'clean_script_tag' );
+
+add_filter( 'style_loader_src', 'remove_cssjs_ver' );
+
+add_filter( 'script_loader_src', 'remove_cssjs_ver' );
+
+
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+
+?>
